570a21d9308c5c5e005633fc0a2afe2113c3d370
lworobey <worobeyluke@gmail.com>
2025-01-30 11:07:38 -0500

    moved package json in backend to a higher level and updated login button in frontend. Also added react icons
    Co-authored-by: Julius Sasmito <jujubeege@gmail.com>

--- video-game-db-frontend/pages/Login.jsx
+++ video-game-db-frontend/pages/Login.jsx
@@ -4,19 +4,9 @@ const Login = () => {
 return(
     <div className="Login container">
     <h1>This is the Login page</h1>
-    <input
-      type="text" 
-      placeholder="login"
-      className="login-input"
-    />
-    <input
-      type="text" 
-      placeholder="Password"
-      className="password-input"
-    />
           <div className="login-container">
             <button type="button" className="login-button">
-              Login
+              Login in with Discord
             </button>
           </div>
 
